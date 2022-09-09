84bf12c28bee2b484f9b23ac81e659f9bd05fb66
AshanKaushanka96 <77004913+AshanKaushanka96@users.noreply.github.com>
2022-09-09 10:22:51 +0530

    Add files via upload

---
+++ all_in_one_watcher.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3.9
+from curses import echo
+import datetime
+import configparser
+#import send_mail 
+import time
+import subprocess
+import logging
+from time import sleep
+parser = configparser.ConfigParser()
+parser.read('config.ini')
+logging.basicConfig(filename='warning.log', level=logging.DEBUG)
+def checkIfProcessNotRunning(processname):
+   process = subprocess.Popen("ps x|grep -ai {} |grep -v grep |wc -l".format(processname),
+                           shell=True, stdout=subprocess.PIPE,
+                           universal_newlines=True)
+   output=int(process.stdout.read())
+   if output > 0:
+    return False
+   else:
+    return True
+
+while True:    
+ for section_name in parser.sections():
+    value = parser.items(section_name)
+    name = value[0][1]
+    starttime = value[1][1]
+    stoptime = value[2][1]
+    weekday =value[3][1]
+    description = value[4][1]
+    needToUp = value[5][1]
+    try:
+        runScriptPath = value[6][1]
+    except:
+        continue
+    currenttime = datetime.datetime.now().strftime("%H:%M:%S")
+    weekdaytime = datetime.datetime.now().weekday()
+
+    if (str(weekdaytime) in str(weekday)):
+        if starttime < currenttime and currenttime < stoptime:
+            if checkIfProcessNotRunning(name):
+                if needToUp == 'Yes':
+                    subprocess.call(['sh', runScriptPath])
+                with open('watcher-log.txt', 'a') as f:
+                    f.write(currenttime + ":" + name + " is not running")
+                    f.write('\n')
+
+    time.sleep(20)
+        
