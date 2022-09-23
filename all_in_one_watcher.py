d320e31d86cecb2edd0e66d5d187e28251534714
ashankaushanka96 <77004913+ashankaushanka96@users.noreply.github.com>
2022-09-23 14:42:17 +0530

    Update all_in_one_watcher.py

--- all_in_one_watcher.py
+++ all_in_one_watcher.py
@@ -20,15 +20,19 @@ def checkIfProcessNotRunning(processname):
     return False
    else:
     return True
+def logger(name,currenttime):
+   with open('watcher-log.txt', 'a') as f:
+      f.write(currenttime + ":" + name + " is not running")
+      f.write('\n')
 
 while True:    
  for section_name in parser.sections():
     value = parser.items(section_name)
-    name = value[0][1]
+    tag = value[0][1]
     starttime = value[1][1]
     stoptime = value[2][1]
     weekday =value[3][1]
-    description = value[4][1]
+    name = value[4][1]
     needToUp = value[5][1]
     try:
         runScriptPath = value[6][1]
@@ -40,20 +44,16 @@ while True:
 
     if (str(weekdaytime) in str(weekday)):
         if starttime < currenttime and currenttime < stoptime:
-            if checkIfProcessNotRunning(name):
+            if checkIfProcessNotRunning(tag):
                 if needToUp == 'Yes':
+                     logger(name,currenttime)
                      mail_send(name)
                      os.chdir(runScriptPath)
                      subprocess.call(['sh', runScript])
-                     time.sleep(20) 
-                     with open('watcher-log.txt', 'a') as f:
-                        f.write(currenttime + ":" + name + " is not running")
-                        f.write('\n')
+                     time.sleep(20)
                 else:
+                     logger(name,currenttime)
                      mail_send(name)
-                     with open('watcher-log.txt', 'a') as f:
-                        f.write(currenttime + ":" + name + " is not running")
-                        f.write('\n')
 
     
         
