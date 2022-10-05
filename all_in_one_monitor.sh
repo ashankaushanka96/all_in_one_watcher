e2c3802a4a8a841199c625bc1b106044a04e86cf
ashankaushanka96 <77004913+ashankaushanka96@users.noreply.github.com>
2022-10-05 12:45:03 +0530

    Update all_in_one_monitor.sh

--- all_in_one_monitor.sh
+++ all_in_one_monitor.sh
@@ -3,7 +3,7 @@
 numproc=`ps x | grep -ai all_in_one_watcher.py | grep -v "grep" | wc -l`
 if [ $numproc -lt 1 ]
 then
-cd /home/ashan/app/WaldermortBotFinal
+cd /home/directfn/app/all_in_one_watcher
 nohup python3 all_in_one_watcher.py > logs/all_in_one_watcher.out 2> logs/all_in_one_watcher.err
 fi
 
