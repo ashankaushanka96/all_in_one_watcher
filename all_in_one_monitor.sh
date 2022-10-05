5a3f536f6d87096ea933c33f29d990a0a965b80b
ashankaushanka96 <77004913+ashankaushanka96@users.noreply.github.com>
2022-10-05 12:43:39 +0530

    Create all_in_one_monitor.sh

---
+++ all_in_one_monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+numproc=`ps x | grep -ai all_in_one_watcher.py | grep -v "grep" | wc -l`
+if [ $numproc -lt 1 ]
+then
+cd /home/ashan/app/WaldermortBotFinal
+nohup python3 all_in_one_watcher.py > logs/all_in_one_watcher.out 2> logs/all_in_one_watcher.err
+fi
+
