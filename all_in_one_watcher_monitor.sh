1bb2de0e13524a7caa37d858d4665a51770a51d2
ashankaushanka96 <77004913+ashankaushanka96@users.noreply.github.com>
2022-10-14 10:53:45 +0530

    Update all_in_one_watcher_monitor.sh

--- all_in_one_watcher_monitor.sh
+++ all_in_one_watcher_monitor.sh
@@ -4,6 +4,6 @@ numproc=`ps x | grep -ai all_in_one_watcher.py | grep -v "grep" | wc -l`
 if [ $numproc -lt 1 ]
 then
 cd /home/directfn/app/all_in_one_watcher
-nohup python3 all_in_one_watcher.py > logs/all_in_one_watcher.out 2> logs/all_in_one_watcher.err &
+./all_in_one_watcher_run.sh
 fi
 
